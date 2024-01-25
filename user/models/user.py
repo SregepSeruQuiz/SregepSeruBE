382927cddfbf42b6be2c4e8c6e05ee3bad9ede0a
DanielHartanto21 <daniel.hartanto.70@gmail.com>
2024-01-25 17:39:22 +0700

    user table

---
+++ user/models/user.py
@@ -0,0 +1,15 @@
+from core.core import meta
+
+from sqlalchemy import Table, Column
+
+from sqlalchemy import BigInteger, Unicode, Boolean
+
+UserModel: Table = Table(
+    "User",meta,
+    Column("id",BigInteger,primary_key=True),
+    Column("uuid",Unicode(256), nullable=False, unique=True),
+    Column("username",Unicode(256), nullable=False, unique=True),
+    Column("email",Unicode(256), nullable=False, unique=True),
+    Column("password",Unicode(256), nullable=False, unique=True),
+    Column("reviewer",Boolean, nullable=False, unique=True,default=False)
+)
