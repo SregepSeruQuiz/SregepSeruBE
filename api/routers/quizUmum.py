363e1243fcec5ed00215795984abec8e9b18ba61
DanielHartanto21 <daniel.hartanto.70@gmail.com>
2024-02-05 22:04:45 +0700

    user router dikit

--- api/routers/quizUmum.py
+++ api/routers/quizUmum.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncConnection
 from typing import Optional
 
 from quiz.exceptions import QuizNotFoundError
-from api.depends.login import
+from api.depends.login import get_user_id
 
 router_quiz = APIRouter(prefix="/quiz", tags=["Quiz"])
 
--- api/routers/user.py
+++ api/routers/user.py
@@ -9,9 +9,10 @@ from jose import jwt
 from api.config import cfg
 from api.depends.db import get_conn
 from api.depends.login import get_uuid
+from api.depends.login import get_user_id
 from user.facades.user import User
 from user.exceptions import EmailNotFoundError, WrongPasswordError
-
+from user.schemas.user import UserAddSchema
 router_login = APIRouter(tags=['login'])
 
 context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -41,3 +42,40 @@ async def login_email(*, conn: AsyncConnection = Depends(get_conn), data: OAuth2
         raise HTTPException(status_code=406, detail="Incorrect password")
     except IntegrityError as e:
         raise HTTPException(status_code=404, detail="Email not found ")
+
+@router_login.post("/create_user")
+async def create_user(*,conn: AsyncConnection = Depends(get_conn), data:UserAddSchema):
+    """
+    router untuk membuat user baru
+    :param conn:
+    :param data:
+    :return:
+    """
+    u = User(conn)
+    try:
+        result = await u.input(data)
+        return result
+    except IntegrityError as e:
+        raise HTTPException(status_code=404, detail="Email not found ")
+
+@router_login.post("/profile")
+async def profile(*,conn_id:AsyncConnection = Depends(get_user_id)):
+    """
+    router untuk melihat profile diri sendiri
+    :param conn:
+    :return:
+    """
+    conn = conn_id[0]
+    id = conn_id[1]
+    u = User(conn)
+    result = await u.profile(id)
+    return result
+
+@router_login.update("/update_email")
+async def update_email(*,conn_id:AsyncConnection = Depends(get_user_id),email:str):
+    """
+    router untuk change email
+    :param conn_id:
+    :param email:
+    :return:
+    """
